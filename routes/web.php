ab819248bbf45bc7e8532ff4b276cd8ad517a902
iampitufo <tenikaler.smith@utp.ac.pa>
2021-06-18 14:21:09 -0500

    TODO: Cambios en la ruta,  controlador y vista

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Controller::class, "index"])->name("index");
