f3ee70441337334aa8124d267a4929a04500fb0d
iampitufo <tenikaler.smith@utp.ac.pa>
2021-06-19 22:50:53 -0500

    modelos y controladores

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Rol extends Model
+class Usuario extends Model
 {
-    protected $table = 'roles';
     use HasFactory;
 }
--- app/Models/UsuarioRol.php
+++ app/Models/UsuarioRol.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Rol extends Model
+class UsuarioRol extends Model
 {
-    protected $table = 'roles';
+    protected $table = 'usuarios_roles';
     use HasFactory;
 }
