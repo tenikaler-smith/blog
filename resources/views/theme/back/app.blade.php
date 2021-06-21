295008f622ad768527b1423bf85e1e06b8731fe5
iampitufo <tenikaler.smith@utp.ac.pa>
2021-06-21 01:35:02 -0500

    TODO: Add file resources view

--- resources/views/theme/back/app.blade.php
+++ resources/views/theme/back/app.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,13 +10,11 @@
     <meta name="description"
         content="Matrix Admin Lite Free Version is powerful and clean admin dashboard template, inpired from Bootstrap Framework">
     <meta name="robots" content="noindex,nofollow">
-    <title>Matrix Admin Lite Free Versions Template by WrapPixel</title>
+    <title>@yield('title', 'mi blog') - BlogTeñi</title>
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset("assets/back/images/favicon.png")}}">
     <!-- Custom CSS -->
-    <link href="../../assets/libs/flot/css/float-chart.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="../../dist/css/style.min.css" rel="stylesheet">
+    <link href="{{asset("assets/back/css/style.min.css")}}" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -27,6 +24,97 @@
 </head>
 
 <body>
+
+
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full"
+        data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+
+        @include('theme.back.top_header')
+
+        <!-- ============================================================== -->
+        <!-- End Topbar header -->
+        <!-- ============================================================== -->
+
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+
+        @include('theme.back.aside')
+
+        <!-- ============================================================== -->
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+
+                @yield('contenido')
+
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+            @include('theme.back.footer')
+            <!-- ============================================================== -->
+            <!-- End footer -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End page wrapper  -->
+        <!-- ============================================================== -->
+
+    </div>
+    <!-- ============================================================== -->
+    <!-- End wrapper  -->
+    <!-- ============================================================== -->
+
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="{{asset("assets/back/libs/jquery/dist/jquery.min.js")}}"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="{{asset("assets/back/libs/bootstrap/dist/js/bootstrap.bundle.min.js")}}"></script>
+    <script src="{{asset("assets/back/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js")}}"></script>
+    <script src="{{asset("assets/back/extra-libs/sparkline/sparkline.js")}}"></script>
+    <!--Wave Effects -->
+    <script src="{{asset("assets/back/js/waves.js")}}"></script>
+    <!--Menu sidebar -->
+    <script src="{{asset("assets/back/js/sidebarmenu.js")}}"></script>
+    <!--Custom JavaScript -->
+    <script src="{{asset("assets/back/js/custom.min.js")}}"></script>
+    <!--This page JavaScript -->
+    <!-- <script src="../../dist/js/pages/dashboards/dashboard1.js"></script> -->
+    <!-- Charts js Files -->
+    <script src="{{asset("assets/back/libs/flot/excanvas.js")}}"></script>
+    <script src="{{asset("assets/back/libs/flot/jquery.flot.js")}}"></script>
+    <script src="{{asset("assets/back/libs/flot/flot.pie.js")}}"></script>
+    <script src="{{asset("assets/back/libs/flot/jquery.flot.time.js")}}"></script>
+    <script src="{{asset("assets/back/libs/flot/jquery.flot.stack.js")}}"></script>
+    <script src="{{asset("assets/back/libs/flot/jquery.flot.crosshair.js")}}"></script>
+    <script src="{{asset("assets/back/libs/flot.tooltip/js/jquery.flot.tooltip.min.js")}}"></script>
+    <script src="{{asset("assets/back/js/pages/chart/chart-page-init.js")}}"></script>
 
 </body>
 
---
+++ resources/views/theme/back/footer.blade.php
@@ -0,0 +1,4 @@
+            <footer class="footer text-center">
+                Todos los derechos resevados TeñiSmith. Blog <a
+                    href="https://www.tenismith.com">Teñi Smith</a>.
+            </footer>
